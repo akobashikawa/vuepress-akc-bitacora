214886d7742fadf8a000fbd801026efb69377002
Antonio Kobashikawa <akobashikawa@gmail.com>
2023-03-08 09:03:22 +0000

    Updates

--- assets/js/9.c55ad3ab.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{448:function(s,t,n){"use strict";n.r(t);var i={data:function(){return{msg:"Hello this is <demo-component>"}}},e=n(71),o=Object(e.a)(i,(function(){return(0,this._self._c)("p",{staticClass:"demo"},[this._v("\n  "+this._s(this.msg)+" "+this._s(2)+"\n")])}),[],!1,null,null,null);t.default=o.exports}}]);
